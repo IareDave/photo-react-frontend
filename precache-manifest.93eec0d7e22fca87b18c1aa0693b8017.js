2ab22b07cdbff635d9b11a5aaead64ac1be2cd85
IareDave <gouletdavid95@gmail.com>
2019-04-26 08:39:21 -0400

    Updates

---
+++ precache-manifest.93eec0d7e22fca87b18c1aa0693b8017.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "625e4b3642da2d6b5bf832d4a6182155",
+    "url": "/photo-react-frontend/index.html"
+  },
+  {
+    "revision": "406034dd8b0a1bcd9f80",
+    "url": "/photo-react-frontend/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/photo-react-frontend/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "406034dd8b0a1bcd9f80",
+    "url": "/photo-react-frontend/static/js/main.447aca6a.chunk.js"
+  },
+  {
+    "revision": "8080ebfc52a3da2d0f97",
+    "url": "/photo-react-frontend/static/js/runtime~main.523af147.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/photo-react-frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
